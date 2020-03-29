99c52419df9a2f55dbd83712da20b8af350c1e59
Araik Rasoyan <araikrasoian@gmail.com>
2020-03-29 23:14:48 +0300

    fix: responsive

--- script.js
+++ script.js
@@ -8,16 +8,6 @@ const BUTTON_CLOSE = document.getElementById('form-message-block-close-JS'); // 
 
 
 // Header
-MENU.addEventListener('click', (event) => {
-  MENU.querySelectorAll('.header-navigation-list__item .header-navigation-list__link').forEach(element => element.classList.remove('header-navigation-list__link-active')); // пробежаться по списку и поудалять
-
-  if(~event.target.className.indexOf('header-navigation-list__link', 0)) // если таргет был именно на ссылку (~ = -1)
-	event.target.classList.add('header-navigation-list__link-active');  // добавить класс
-});
-
-
-
-
 
 
 
@@ -41,10 +31,7 @@ window.addEventListener('scroll', function() {
 
 	 }
 	 
-
 	 });
-
-
 
   });
 
